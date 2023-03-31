413cc64f37d8f12a72496cde82215cb9115462aa
Scott Watermasysk <scottwater@gmail.com>
2023-03-30 23:13:07 -0400

    note on where to see the filter in action

--- plugins/builders/test.rb
+++ plugins/builders/test.rb
@@ -1,5 +1,6 @@
 class Builders::Test < SiteBuilder
   def build
+    # NOTE: This filter is used on index.md
     liquid_filter :multiply_and_optionally_add do |input, multiply_by, add_by = nil|
       value = input * multiply_by
       add_by ? value + add_by : value
